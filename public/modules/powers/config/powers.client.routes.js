937764cf8ed1290cc4f65521d1834ee07d83a6c6
Shawn Lin <jallen0927@hotmail.com>
2015-04-05 02:47:32 +1200

    Add pages route and view

--- public/modules/powers/config/powers.client.routes.js
+++ public/modules/powers/config/powers.client.routes.js
@@ -6,10 +6,31 @@
 //Setting up route
 angular.module('powers').config(['$stateProvider',
 	function($stateProvider){
-		// PM state routing
-		$stateProvider.state('profile', {
+		// Powers state routing
+		$stateProvider.
+		state('comparePowers', {
 			url: '/power/compare',
-			templateUrl: 'modules/pm/views/power-compare.client.view.html'
+			templateUrl: 'modules/powers/views/compare-powers.client.view.html'
+		}).
+		state('signupPowers', {
+			url: '/power/signup',
+			templateUrl: 'modules/powers/views/signup-powers.client.view.html'
+		}).
+		state('hardwarePowers', {
+			url: '/power/hardware',
+			templateUrl: 'modules/powers/views/hardware-powers.client.view.html'
+		}).
+		state('blogPowers', {
+			url: '/power/blog',
+			templateUrl: 'modules/powers/views/blog-powers.client.view.html'
+		}).
+		state('aboutusPowers', {
+			url: '/power/aboutus',
+			templateUrl: 'modules/powers/views/aboutus-powers.client.view.html'
+		}).
+		state('contactusPowers', {
+			url: '/power/contactus',
+			templateUrl: 'modules/powers/views/contactus-powers.client.view.html'
 		});
 	}
 ]);
